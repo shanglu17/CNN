2ee786291afd3f9374fef4552b4b19708f57fed4
shanglu17 <155158407+shanglu17@users.noreply.github.com>
2025-05-12 19:27:28 +0800

    new version

---
+++ CNN.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.nn.utils import prune
+import os
+
+#
